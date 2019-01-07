48e920b490c267b294f11126580bed32acd2e40c
Diana Fechner <Diana.Fechner@fh-erfurt.de>
2019-01-07 21:48:21 +0100

    added worlds edit

---
+++ src/app/worlds/edit/edit.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.css']
+})
+export class EditComponent implements OnInit {
+
+  private worldsname : string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public onSubmit(f: NgForm){
+
+  }
+
+}
--- src/app/worlds/worlds.module.ts
+++ src/app/worlds/worlds.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index/index.component';
 
 import{RouterModule, Routes}  from '@angular/router';
+import { EditComponent } from './edit/edit.component';
+
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {path: '', component: IndexComponent},
+  {path: 'edit', component: EditComponent},
 ];
 
 @NgModule({
-  declarations: [IndexComponent],
+  declarations: [IndexComponent, EditComponent],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes),
   ]
 })
